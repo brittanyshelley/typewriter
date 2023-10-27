e85076c74807f06eead79657cc73508a0457a753
brittanyshelley <brittany.j.shelley@gmail.com>
2023-10-26 20:41:49 -0600

    adjusted function

--- typewriter.js
+++ typewriter.js
@@ -1,11 +1,19 @@
 
 const sentence = "hello there from lighthouse labs";
+
 const typewriter = function(string) {
+  let delay = 0;
   for (const char of string) {
     setTimeout(function() {
-       process.stdout.write(char)// print the char here
-    }, 1000) // <= 1s delay to make it noticeable. Can dial it down later.
+      process.stdout.write(char);
+    }, delay);
+    //adjust delay for each character.
+    delay += 50;
   }
+  setTimeout(() => {
+    //add a new line after the last character.
+    process.stdout.write("\n");
+  }, delay);
 };
 
 
